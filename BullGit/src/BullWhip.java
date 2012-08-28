79cfdf6952adec209987aeb6ceee1ce632c9e34c
Alex Phung <alex.phung@intercax.com>
2012-08-28 12:29:24 -0400

    HELLO-5 #Start Progress let's try one more time.

--- BullGit/src/BullWhip.java
+++ BullGit/src/BullWhip.java
@@ -15,9 +15,11 @@ public class BullWhip {
 	
 	public static void main(String[] args) {
 		System.out.println("BullWhip");
-
+		//
 		BullWhip.whipIt();
+		//
 		BullWhip.whipItGood();
+		//
 		BullWhip.whipItRealGood();
 	}
 
